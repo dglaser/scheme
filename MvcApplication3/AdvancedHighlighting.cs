dde3b8bb17b41ac8c706424c88746db1f7a4e3cb
Dale Glaser <dglaser@esc12.net>
2014-09-15 09:50:58 -0500

    initial files

---
+++ MvcApplication3/AdvancedHighlighting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication3
+{
+    public class Highlighting {
+    string inactiveSelection = "select this";
+    string highlightedRef;
+    Collapsed text
+    public void CollapsibleRegion() {
+        string warning = "assigned but not used";
+    }
+    public void Features() {
+        highlightedRef = "again";
+        var break = "breakpoint";
+        SyntaxError();
+        CompilerError();
+#if UNDEFINED_SYMBOL
+        object excludedCode = null;
+#endif
+        break = "current statement";
+    }
+}
+}
